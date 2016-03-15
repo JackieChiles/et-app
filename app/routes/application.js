616a0fb51a281bd2cb2381f16ef9365443e23855
Drew Gaynor <drew.gaynor@inin.com>
2016-03-15 15:25:13 -0400

    Fixed application route.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,9 @@
+import Ember from 'ember';
+
 export default Ember.Route.extend({
     intl: Ember.inject.service(),
     beforeModel() {
-        //return this.get('intl').setLocale('en-us');
+        return this.get('intl').setLocale('en-us');
         //return this.get('intl').setLocale('en-gb');
-        return this.get('intl').setLocale('es');
     }
 });
